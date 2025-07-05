b36572a45cf89e1786499daa14a177a4dc7d48d6
mohroua <ibrahimbiskra1971@gmail.com>
2025-07-05 18:52:30 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
 const CACHE_NAME = "budget-cache-v1";
 const urlsToCache = [
-  "/index.html",
-  "/style.css",
-  "/app.js",
-  "/manifest.json",
-  "/fontawesome/css/all.min.css",
-  "/fontawesome/webfonts/fa-solid-900.woff2",
-  "/fontawesome/webfonts/fa-regular-400.woff2",
-  "/fonts/Cairo.woff2",
-  "/libs/chart.min.js",
-  "/libs/jspdf.umd.min.js"
+  "index.html",
+  "style.css",
+  "app.js",
+  "manifest.json",
+  "fontawesome/css/all.min.css",
+  "fontawesome/webfonts/fa-solid-900.woff2",
+  "fontawesome/webfonts/fa-regular-400.woff2",
+  "fonts/Cairo.woff2",
+  "libs/chart.min.js",
+  "libs/jspdf.umd.min.js"
 ];
 
 self.addEventListener("install", (event) => {
